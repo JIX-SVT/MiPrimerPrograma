1c8210442867ae6c1309e1f5720573a36c8b9e27
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-04-21 19:36:14 -0600

    003 Se agrego la estructura de control switch

--- src/org/jafethperez/view/ViewMenuPrincipal.java
+++ src/org/jafethperez/view/ViewMenuPrincipal.java
@@ -2,23 +2,38 @@ package org.jafethperez.view;
 import java.util.Scanner;
 
 public class ViewMenuPrincipal {
-    //Global 
+    //Global
     int opcionMenu = 0;
     Scanner leer = new Scanner(System.in);
-    
-    public void mostrarMenu(){
-     //Local
-     while (opcionMenu == 0){
-         System.out.println("Bienvenido,seleccione una opción:");
-         System.out.println("1. Operaciones matemáticas");
-         System.out.println("2. Operaciones de texto");
-         System.out.println("3. Operaciones de lógica");
-         System.out.println("4. Salir del sistema");
-         opcionMenu = Integer.parseInt(leer.nextLine());   
-     }
-    }
-    
-    public void mostrarMenuMatematica(){
-        System.out.println("Menu de operaciones matemáticas");
+
+    public void mostrarMenu() {
+        while (opcionMenu == 0) {
+            System.out.println("Bienvenido al: ");
+            System.out.println("--- MENU PRINCIPAL ---");
+            System.out.println("1. Operaciones matemáticas");
+            System.out.println("2. Operaciones de texto");
+            System.out.println("3. Operaciones de lógica");
+            System.out.println("4. Salir");
+            System.out.print("Ingresa la opción: ");
+            
+            opcionMenu = Integer.parseInt(leer.nextLine());
+
+            switch (opcionMenu) {
+                case 1:
+                    mostrarMenuMatematica(); 
+                    break; 
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4: System.out.println("Saliendo..");
+                    opcionMenu = 4;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }
     }
 }
